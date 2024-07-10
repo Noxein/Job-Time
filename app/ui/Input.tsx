55b8840ab1df58eee0251c7ae49512fa311aa3e7
Noxein <138109221+Noxein@users.noreply.github.com>
2024-07-10 11:53:42 +0200

    Fixed wrong input width on mobile

--- app/ui/Input.tsx
+++ app/ui/Input.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export const Input = ({nazwa,name,type}:{nazwa:string,name:string,type:string}) => {
     return(
-        <>  
+        <div className='w-full flex flex-col'>  
             <label htmlFor={name}>{nazwa}</label>
             <input type={type} id={name} name={name} className="border-2 rounded h-12"/>
-        </>
+        </div>
     )
 }
 
