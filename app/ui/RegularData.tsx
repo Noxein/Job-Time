0bea78f893c6c42547848efe0a3538a48c4c5c20
Noxein <138109221+Noxein@users.noreply.github.com>
2024-07-15 18:42:34 +0200

    centered the div

--- app/ui/RegularData.tsx
+++ app/ui/RegularData.tsx
@@ -3,13 +3,13 @@ import { NameSurename } from './NameSurename'
 
 export const RegularData = () => {
   return (
-    <div className='ml-2 md:ml-0 max-w-7xl flex mx-auto mt-10 mb-10'>
+    <div className='mt-10 pl-2 flex mx-auto max-w-7xl '>
         <div className='w-6/12'>
             <p>Renata Kowalczyk Roboty Ziemne</p>
             <p>Anielów 26, 08-460 Sobolew</p>
             <p>NIP 826-192-64-10, REGON 521299369</p>
         </div>
-        <div className='w-5/12 flex'>
+        <div className='w-6/12 flex'>
             <div className='flex flex-col'>
                 <b>Karta czasu pracy operatora</b>
                 <div className='mt-4'>Kierowca</div>
