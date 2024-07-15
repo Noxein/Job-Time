f21179c515867658e581052155047ed1c979cceb
Noxein <138109221+Noxein@users.noreply.github.com>
2024-07-15 18:41:37 +0200

    added refs, for printing feature

--- app/ui/DispalyDays.tsx
+++ app/ui/DispalyDays.tsx
@@ -7,36 +7,49 @@ import { DispalyDayOnPhone } from './DispalyDayOnPhone'
 import { DispalyDayOnPhoneWithData } from './DispalyDayOnPhoneWithData'
 import { TableRefContext } from '../context/PhoneTableRefContext'
 
-export async function PrintPage(){
-  const nav = document.querySelector('nav')
-  const printButton = document.querySelector('#printButton')
-  
-  nav?.classList.add('hidden')
-  printButton?.classList.add('hidden')
 
-  window.print()
-  nav?.classList.remove('hidden')
-  printButton?.classList.remove('hidden')
 
-}
-
-export const DispalyDays = ({days,setIdToRemove,showDownloadBtn=false}:{days:DayType[],setIdToRemove?:React.Dispatch<React.SetStateAction<string[]>>,showDownloadBtn?:boolean}) => {
+export const DispalyDays = ({days,setIdToRemove,showDownloadBtn=false}:{days?:DayType[],setIdToRemove?:React.Dispatch<React.SetStateAction<string[]>>,showDownloadBtn?:boolean}) => {
   const tableRef = useContext(TableRefContext)
+  const buttonRef = useRef<HTMLButtonElement|null>(null)
+
+  async function PrintPage(){
+    const nav = document.querySelector('nav')
+    const printButton = buttonRef.current
+    const MobileTableRef = tableRef?.MobileTableRef.current
+    const DesktopTableRef = tableRef?.DesktopTableRef.current
+
+    MobileTableRef?.classList.add('hidden')
+    DesktopTableRef?.classList.remove('hidden')
+    
+    nav?.classList.add('hidden')
+    printButton?.classList.add('hidden')
+  
+    window.print()
+
+    MobileTableRef?.classList.remove('hidden')
+    DesktopTableRef?.classList.add('hidden')
+
+    nav?.classList.remove('hidden')
+    printButton?.classList.remove('hidden')
+  
+  }
 
   return (
     <>
-    <table className=' hidden lg:block justify-center max-w-7xl mx-auto' >
+    <table className=' hidden lg:block max-w-7xl mx-auto printTable mt-10' ref={tableRef?.DesktopTableRef}>
       <DisplayDaysHead />
-      {days.map((day,index)=>(<DisaplyDay key={day.id} day={day} index={index}/>))}
+      {days && days.map((day,index)=>(<DisaplyDay key={day.id} day={day} index={index}/>))}
      
     </table>
-    <table className='flex lg:hidden overflow-x-auto whitespace-nowrap w-svw mt-6 phone-table' ref={tableRef?.tableRef}>
+  
+    <table className='flex lg:hidden overflow-x-auto whitespace-nowrap w-svw mt-6 phone-table' ref={tableRef?.MobileTableRef}>
       <DispalyDayOnPhone/>
-      {days.map((day,index)=>(<DispalyDayOnPhoneWithData key={day.id} day={day} index={index}/>))}
+      {days && days.map((day,index)=>(<DispalyDayOnPhoneWithData key={day.id} day={day} index={index}/>))}
     </table>
     
     <div className='flex justify-center mt-10'>
-      {showDownloadBtn && <button onClick={()=>{PrintPage()}} id='printButton' className='p-4 border-2 hover:border-neutral-800 rounded-xl' >Pobierz tabele</button>}
+      {showDownloadBtn && <button onClick={()=>{PrintPage()}} ref={buttonRef} className='p-4 hover:border-blue-100 rounded-xl border-blue-400 border-4 ' >Pobierz tabele</button>}
     </div>
    
     
