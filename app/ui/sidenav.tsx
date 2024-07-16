5dc6422636b87a3a8a07b89327510340bf7d721e
Noxein <138109221+Noxein@users.noreply.github.com>
2024-07-16 21:05:27 +0200

    fixed animation to work only on mobile

--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -2,14 +2,21 @@
 import Link from 'next/link'
 import React, { useContext, useRef, useState } from 'react'
 import { SideNavContext } from '../context/SideNavContext'
+import useWindowDimensions from './useWidth'
 
 export default function Sidenav({logoutBtn}:{logoutBtn:React.ReactNode}){
   const hover = 'hover:bg-blue-700 p-2 rounded'
   const style = 'bg-blue-600 w-full text-center border-blue-50 box-border lg:w-max'
   const sideNavContext = useContext(SideNavContext)
   const { isOpen, setIsOpen } = sideNavContext!
+  const{ width } = useWindowDimensions()
 
-  const OpenNav = isOpen? 'OpenSidenav' : 'OpenSidenavReverse'
+  function shouldAnimateNavbar(){
+    if(width>768) return ''
+    if(isOpen) return 'OpenSidenav'
+    return 'OpenSidenavReverse'
+  }
+  const OpenNav = shouldAnimateNavbar()
   return (<>
     {!isOpen && <button className='fixed md:hidden' onClick={()=>setIsOpen(x=>!x)}>-=+</button>}
     <nav className={`px-0 lg:px-24 flex flex-col fixed gap-6 h-dvh ${OpenNav} md:static md:h-16 md:flex-row md:w-screen bg-blue-500 text-white items-center text-l px-6 z-10`}>
