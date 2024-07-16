bfa95f57b0ea57aa86016298e12e5665712f1bae
Noxein <138109221+Noxein@users.noreply.github.com>
2024-07-16 21:19:23 +0200

    fixed animation issue on desktop

--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -11,33 +11,32 @@ export default function Sidenav({logoutBtn}:{logoutBtn:React.ReactNode}){
   const { isOpen, setIsOpen } = sideNavContext!
   const{ width } = useWindowDimensions()
 
-  function shouldAnimateNavbar(){
-    if(width>768) return ''
-    if(isOpen) return 'OpenSidenav'
-    return 'OpenSidenavReverse'
+  function ClickNavbarLinkOnMobile(){
+    if(width>768) return
+    setIsOpen(false)
   }
-  const OpenNav = shouldAnimateNavbar()
+  const OpenNav = isOpen?'OpenSidenav':'OpenSidenavReverse'
   return (<>
     {!isOpen && <button className='fixed md:hidden' onClick={()=>setIsOpen(x=>!x)}>-=+</button>}
     <nav className={`px-0 lg:px-24 flex flex-col fixed gap-6 h-dvh ${OpenNav} md:static md:h-16 md:flex-row md:w-screen bg-blue-500 text-white items-center text-l px-6 z-10`}>
       
         <button onClick={()=>setIsOpen(x=>!x)} className='absolute left-0 md:hidden'> +=- </button>
-        <Link href={'/home'} className={`${hover} mt-10 md:mt-0 ${style}`} onClick={()=>setIsOpen(x=>!x)} tabIndex={isOpen?0:-1}>
+        <Link href={'/home'} className={`${hover} mt-10 md:mt-0 ${style}`} tabIndex={isOpen?0:-1} onClick={ClickNavbarLinkOnMobile}>
           Strona Glówna
         </Link>
 
         <Link href={'/home/szukaj'} tabIndex={isOpen?0:-1}
-        className={`${hover} ${style}`} onClick={()=>setIsOpen(x=>!x)}>
+        className={`${hover} ${style}`} onClick={ClickNavbarLinkOnMobile}>
         Szukaj po dacie
         </Link>
 
         <Link href={'/home/add'} tabIndex={isOpen?0:-1}
-        className={`${hover} ${style}`} onClick={()=>setIsOpen(x=>!x)}>
+        className={`${hover} ${style}`} onClick={ClickNavbarLinkOnMobile}>
         Dodaj rekord
         </Link>
 
         <Link href={'/home/remove'} tabIndex={isOpen?0:-1}
-        className={`${hover} ${style}`} onClick={()=>setIsOpen(x=>!x)}>
+        className={`${hover} ${style}`} onClick={ClickNavbarLinkOnMobile}>
         Usuń rekord
         </Link>
 
